7509e158ad75a8c2e6c179dc7249b50cf8e27b28
andrewchatch <andrew.hatch96@gmail.com>
2024-02-16 09:03:25 -0700

    Shop file

--- src/app/shop/components/ShopLink.tsx
+++ src/app/shop/components/ShopLink.tsx
@@ -1,14 +1,35 @@
+'use client'
+
 import Book from "@/app/components/home/Book";
 import Image from "next/image";
 import Link from "next/link";
+import React, { useState } from "react";
 
 const ShopLink = (props: {book: Book}) => {
     const { book } = props;
+    const [isVisible, setIsVisible] = useState(false);
+    
+    const toggleVisibility = (event: React.MouseEvent) => {
+        event.preventDefault();
+        const target = (event.currentTarget as HTMLElement);
+        const image = (target.firstChild as HTMLElement);
+        const clickMe = (target.childNodes[1] as HTMLElement);
+        if (isVisible) {
+            image.style.opacity = "1";
+            clickMe.style.display = "none";
+            setIsVisible(false);
+        } else {
+            image.style.opacity = "0.5";
+            clickMe.style.display = "block";
+            setIsVisible(true);
+        }
+    }
 
     return (
         <div>
-            <Link href={book.url}>
-                <Image className="shop-links" src={book.cover_image} height={book.img_height} width={book.img_width} alt={book.description} />
+            <Link href={book.url} onMouseEnter={toggleVisibility} onMouseLeave={toggleVisibility}>
+                <Image className="shop-links" src={book.cover_image} height={book.img_height} width={book.img_width} alt={book.description} priority={true} />
+                <p id="click-me">Click Here</p>
                 <p className="book-name">{book.name}</p>
             </Link>
         </div>
