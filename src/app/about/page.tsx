7c672ba312c538b72891d5b557fa69a8019837b1
andrewchatch <andrew.hatch96@gmail.com>
2024-02-18 15:07:01 -0700

    Add mobile formatting

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -3,24 +3,19 @@ import aboutMePic from "../assets/images/about_me.png";
 import '../css/About.css';
 
 const About = (props: {}) => {
-    const sizeMult = 0.3
-
     return (
         <div id="about-me-container">
-            <div id="title-container">
-                <h1 id="about-me-title">About</h1>
-            </div>
+            <div id="title-container"></div>
 
             <div id="about-me-section">
                 <Image 
                     id="about-me-image"
                     src={aboutMePic}
-                    height={1920 * sizeMult}
-                    width={1080 * sizeMult}
                     priority={true}
                     alt="Pictures of H.J. Cartwright" />
 
                 <div id="about-me-text">
+                    <h1 id="about-me-title">About</h1>
                     <p>I was born and raised in a small town in Utah. With that comes a hick accent and a million catch-phrases. I currently live with my husband, our two cats and an unnecessary amount of house plants.</p>
                     <p>I hold a BS in Political Science as well as minors in Marketing and Management. I did most of my college career during the pandemic and was able to finish my degree at only twenty years old.</p>
                     <p>Life caught up to me and I took a moment to slow down. During that time I fell back in love with reading and it struck the need to create so I began writing. Before I knew it, I finished my first book, Diluted Truths.</p>
