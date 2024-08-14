fd242eea105587a8c2e32567e6784b0d405f2529
drewcartwright-sunnystreet <drew.cartwright@sunnystreet.com>
2024-08-13 21:10:12 -0600

    Update color scheme

--- src/app/components/home/HomePage.tsx
+++ src/app/components/home/HomePage.tsx
@@ -1,13 +1,21 @@
 import Book from "./Book";
 import NewRelease from "./NewRelease";
-import dilutedTruths from "../../assets/images/diluted_truths.jpg";
+// import dilutedTruths from "../../assets/images/diluted_truths.jpg";
+import followingFortunes from "../../assets/images/following_fortunes.png";
 
 const HomePage = (props: {}) => {
+    // const book: Book = {
+    //     "name": "Diluted Truths\nThe Advocates: Book One",
+    //     "tropes": ["7 Year Age Gap", "Dual POV", "Slow Burn", "Sunshine x Grumpy", "Forbidden Romance"],
+    //     "url": "/shop/diluted_truths",
+    //     "cover_image": dilutedTruths
+    // }
+
     const book: Book = {
-        "name": "Diluted Truths\nThe Advocates: Book One",
-        "tropes": ["7 Year Age Gap", "Dual POV", "Slow Burn", "Sunshine x Grumpy", "Forbidden Romance"],
-        "url": "/shop/diluted_truths",
-        "cover_image": dilutedTruths
+        name: "Following Fortunes\nThe Advocates: Book Two",
+        tropes: ["7 Year Age Gap", "Dual POV", "Slow Burn", "Sunshine x Grumpy", "Forbidden Romance"],
+        url: "/shop/following_fortunes",
+        cover_image: followingFortunes
     }
 
     return (
