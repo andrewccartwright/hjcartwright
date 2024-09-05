292f76906434950dd69390276a042f7dd2aa511e
andrewccartwright <drew.c.cartwright@gmail.com>
2024-09-05 10:10:16 -0600

    Revert "Update for book 2"

--- src/app/components/home/HomePage.tsx
+++ src/app/components/home/HomePage.tsx
@@ -1,21 +1,13 @@
 import Book from "./Book";
 import NewRelease from "./NewRelease";
-// import dilutedTruths from "../../assets/images/diluted_truths.jpg";
-import followingFortunes from "../../assets/images/following_fortunes.png";
+import dilutedTruths from "../../assets/images/diluted_truths.jpg";
 
 const HomePage = (props: {}) => {
-    // const book: Book = {
-    //     "name": "Diluted Truths\nThe Advocates: Book One",
-    //     "tropes": ["7 Year Age Gap", "Dual POV", "Slow Burn", "Sunshine x Grumpy", "Forbidden Romance"],
-    //     "url": "/shop/diluted_truths",
-    //     "cover_image": dilutedTruths
-    // }
-
     const book: Book = {
-        name: "Following Fortunes\nThe Advocates: Book Two",
-        tropes: ["He Falls First", "Marriage of Convenience", "Best Friend's Sister", "Romantic Suspense", "Single Mom"],
-        url: "/shop/following_fortunes",
-        cover_image: followingFortunes
+        "name": "Diluted Truths\nThe Advocates: Book One",
+        "tropes": ["7 Year Age Gap", "Dual POV", "Slow Burn", "Sunshine x Grumpy", "Forbidden Romance"],
+        "url": "/shop/diluted_truths",
+        "cover_image": dilutedTruths
     }
 
     return (
--- src/app/shop/following_fortunes/page.tsx
+++ src/app/shop/following_fortunes/page.tsx
@@ -5,31 +5,7 @@ import BookPage from "../components/BookPage";
 const FollowingFortunes = (props: {}) => {
     const data: BookPageData = {
         image: followingFortunes,
-        summary: `<strong class='names'><i>Theo</i></strong>
-        <br><br>
-        I would do anything for my daughter. 
-        Including marrying my sister's best friend. A man that drove me crazy because he was inexplicably calm. 
-        A man that made me feel cared for in ways I didn't know possible.
-        A man that would help me keep custody of my daughter.
-        <br><br>
-        And just when I thought everything was going right in my life, the Cult of Creatio reared its head and 
-        threatened the most important person to me.
-        <br><br>
-        <em>My daughter.</em>
-        <br><br>
-        <strong class='names'><i>Oliver</i></strong>
-        <br><br>
-        I didn't make rash decisions. I was smart and methodical with my decisions. 
-        But when it came to Theodora James, my best friend's sister, all rational thinking went out the window.
-        <br><br>
-        That's why I proposed on a whim in a coffee shop.
-        <br><br>
-        That's why I married her only a week later.
-        <br><br>
-        But how would I keep her safe when the cult that threatened our family was hidden in 
-        the shadows we were only now bringing to light?
-        <br><br>
-        `,
+        summary: "<strong id='following_fortunes_text'>Coming Soon!</strong>",
         links: [],
         id: "following_fortunes",
         media: []
